62785f5692f6d7ba09712360b630f05634e0a7ca
Pedro Pereira <pedrocjdpereira@ua.pt>
2024-04-29 14:40:52 +0100

    mqtt basicamente done

--- master-node/src/main.py
+++ master-node/src/main.py
@@ -1,17 +1,79 @@
 import os
-import random
 import threading
 import signal
 import time
+import json
 from flask import Flask, render_template
+import paho.mqtt.client as mqtt
+
+# MQTT Broker details
+BROKER_ADDRESS = "mqtt"
+TOPIC = "main"
 
 app = Flask(__name__)
-data = 2
+data = 0
+data_ids = []
+
+RECONNECT_DELAY = 5
+MAX_RECONNECT_COUNT = 10
+
+def on_connect(client, userdata, flags, rc, properties):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+def on_disconnect(client, userdata, rc):
+    print("Disconnected with result code: %s", rc)
+    reconnect_count, reconnect_delay = 0, RECONNECT_DELAY
+    while reconnect_count < MAX_RECONNECT_COUNT:
+        print("Reconnecting in %d seconds...", reconnect_delay)
+        time.sleep(reconnect_delay)
+
+        try:
+            client.reconnect()
+            print("Reconnected successfully!")
+            return
+        except Exception as err:
+            print("%s. Reconnect failed. Retrying...", err)
+
+        reconnect_count += 1
+    print("Reconnect failed after %s attempts. Exiting...", reconnect_count)
+
+def on_message(client, userdata, msg):
+    # Decode the payload and extract the ID
+    try:
+        payload = json.loads(msg.payload.decode("utf-8"))
+        id = payload["id"]
+        operation = payload["operation"]
+        operationState = payload["operationState"]
+        global data
+        global data_ids
+        if operation == "reserve_id" and operationState == "PROCESSING":
+            print("Received reserve ID request {}".format(payload))
+            try:
+                # Check if ID already exists
+                msg = {
+                    "id": id,
+                    "operation": "reserve_id",
+                }
+                if id not in data_ids:
+                    data_ids.append(id)
+                    msg["operationState"] = "COMPLETED"
+                else:
+                    msg["operationState"] = "FAILED"
+                client.publish(TOPIC, json.dumps(msg))
+                print("Published data {}".format(msg))
+            except Exception as e:
+                print("ERROR:{}".format(e))
+        elif operation == "data_transfer":
+            print("Received data {} from table {}".format(payload["data"], id))
+    except Exception as e:
+        print("ERROR: {}".format(e))
 
 @app.route('/', methods=['GET'])
 def root():
     return 'Hello World!'
-    #return render_template('index.html', data=data)
 
 @app.route('/get_data', methods=['GET'])
 def get_data():
@@ -19,14 +81,16 @@ def get_data():
     return str(data)
 
 def start_server():
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=8000)
 
 def start_data():
-    global data
-    while True:
-        data = random.randint(1,10)
-        print(data)
-        time.sleep(2)
+    client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
+    client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
+    client.on_message = on_message
+    client.connect(BROKER_ADDRESS, 1883, 60)
+    client.subscribe(TOPIC)
+    client.loop_forever()
 
 def signal_handler(signal, frame):
     # Handle Ctrl+C
@@ -37,7 +101,7 @@ if __name__ == '__main__':
     # Register the signal handler for Ctrl+C
     signal.signal(signal.SIGINT, signal_handler)
 
-    # Create threads for server and socket
+    # Create threads for server and data and for checking and removing inactive data threads
     server_thread = threading.Thread(target=start_server)
     data_thread = threading.Thread(target=start_data)
 
---
+++ slave-node/src/main.py
@@ -0,0 +1,122 @@
+import os
+import random
+import threading
+import signal
+import time
+import json
+import paho.mqtt.client as mqtt
+
+# MQTT Broker details
+BROKER_ADDRESS = "mqtt"
+TOPIC = "main"
+RECONNECT_DELAY = 5
+MAX_RECONNECT_COUNT = 10
+
+id = -1
+id_accepted = False
+
+def on_connect(client, userdata, flags, rc, properties):
+    if rc == 0:
+        print("Connected to MQTT Broker!")
+    else:
+        print("Failed to connect, return code %d\n", rc)
+
+def on_disconnect(client, userdata, rc):
+    print("Disconnected with result code: %s", rc)
+    reconnect_count, reconnect_delay = 0, RECONNECT_DELAY
+    while reconnect_count < MAX_RECONNECT_COUNT:
+        print("Reconnecting in %d seconds...", reconnect_delay)
+        time.sleep(reconnect_delay)
+
+        try:
+            client.reconnect()
+            print("Reconnected successfully!")
+            return
+        except Exception as err:
+            print("%s. Reconnect failed. Retrying...", err)
+
+        reconnect_count += 1
+    print("Reconnect failed after %s attempts. Exiting...", reconnect_count)
+
+def on_message(client, userdata, msg):
+    global id_accepted
+
+    # Decode the payload and extract the ID
+    try:
+        payload = json.loads(msg.payload)
+        msg_id = payload["id"]
+        operation = payload["operation"]
+        operationState = payload["operationState"]
+    except Exception as e:
+        print("ERROR: {}".format(e))
+
+    if msg_id == id:
+        if operation == "reserve_id":
+            if operationState == "COMPLETED":
+                print("ID {} is accepted.".format(msg_id))
+                id_accepted = True
+            elif operationState == "FAILED":
+                print("ID {} is invalid. Generating a new ID.".format(msg_id))
+                generate_id(client)
+
+def generate_id(client):
+    global id
+
+    id = random.randint(1,10)
+    msg = {
+        "id": id,
+        "operation": "reserve_id",
+        "operationState": "PROCESSING",
+    }
+    client.publish(TOPIC, json.dumps(msg))
+    print("Published data {} to topic {}".format(msg, TOPIC))
+    time.sleep(2)
+
+def start_data():
+    # Create MQTT client
+    client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
+    client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
+    client.on_message = on_message
+    while True:
+        try:
+            client.connect(BROKER_ADDRESS, 1883, 60)
+            break
+        except Exception as e:
+            time.sleep(RECONNECT_DELAY)
+            print("ERROR:{}".format(e))
+    client.subscribe(TOPIC)
+    client.loop_start()
+    
+    while not id_accepted:
+        generate_id(client)
+        time.sleep(2)
+        
+    while True:
+        msg = {
+            "id": id,
+            "operation": "data_transfer",
+            "operationState": "COMPLETED",
+            "data": random.randint(1,8)
+        }
+        client.publish(TOPIC, json.dumps(msg))
+        print("Published msg {}. id = {}".format(msg["data"], id))
+        time.sleep(2)
+
+def signal_handler(signal, frame):
+    # Handle Ctrl+C
+    print("Ctrl+C detected. Exiting...")
+    os._exit(0)  # Terminate the program forcefully
+
+if __name__ == '__main__':
+    # Register the signal handler for Ctrl+C
+    signal.signal(signal.SIGINT, signal_handler)
+
+    # Create thread for data
+    data_thread = threading.Thread(target=start_data)
+
+    # Start the thread
+    data_thread.start()
+
+    # Wait for the thread to complete
+    data_thread.join()
